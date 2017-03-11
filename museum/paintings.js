58aa0296c78287e0a04c342f3f125ae597a0b235
Adityavagarwal <adityavikram.agarwal@research.iiit.ac.in>
2017-03-11 20:54:43 +0530

    Commit hotel, library, museums

---
+++ museum/paintings.js
@@ -0,0 +1,319 @@
+window.GALLERY = window.GALLERY || {};
+
+GALLERY.paintings = [
+{
+    number: 1,
+    src:'alicepic01-500.jpg', 
+    title:'Oasis',
+    description:'Lotus & Bird',
+    dimensions:'16" x 16"'
+},
+{
+    number: 2,
+    src: 'alicepic02-500.jpg',
+    title:'Sending Good Wishes',
+    description:'Award Winning',
+    dimensions:'16" x 16"'
+},
+{
+    number: 3,
+    src: 'alicepic03-500.jpg',
+    title:'Awesome Power Waterfall',
+    description:'Waterfall, care-free style, scholar',
+    dimensions:'16" x 16"'
+},
+{
+    number: 4,
+    src: 'alicepic04-500.jpg',
+    title:'Spring Excursion',
+    description:'Two Mallards',
+    dimensions:'16" x 16"'
+},
+{
+    number: 7,
+    src: 'alicepic07-600.jpg',
+    title:'Under the Silvery Moon',
+    description:'Award Winning.  Inscription: The shadow of bamboo evolves a sense of poetry',
+    dimensions:'18" x 12 ½"'
+},
+{
+    number: 15,
+    src: 'alicepic15-500.jpg',
+    title:'Toy Ducks',
+    description:'Mandarin ducks known as "Love Birds" on Japanese silky rice paper. Note: enriched color.',
+    dimensions:'16" x 16"'
+},
+{
+    number: 18,
+    src: 'alicepic18-500.jpg',
+    title:'Stirring of Hearts',
+    description:'Award Winning',
+    dimensions:'16" x 16"'
+},
+{
+    number: 21,
+    src: 'alicepic21-500.jpg',
+    title:'Etching of Time',
+    description:'Award Winning.  In Private Collection',
+    dimensions:'21" x 14 ¾"'
+},
+{
+    number: 22,
+    src: 'alicepic22-500.jpg',
+    title:'Spring Affection',
+    description:'Commission',
+    dimensions:'16" x 16"'
+},
+{
+    number: 24,
+    src: 'alicepic24-300.jpg',
+    title:'Awesome Finding',
+    description:'Award Winning.  Prints available',
+    dimensions:'11 ¾" x 18 ½"'
+},
+{
+    number: 27,
+    src: 'alicepic27-375.jpg',
+    title:'Poetic Mood',
+    description:'Award Winning.  In Private Collection.  Prints available',
+    dimensions:'16 ¼" x 25 ½"'
+},
+{
+    number: 29,
+    src: 'alicepic29-500.jpg',
+    title:'Welcoming Spring',
+    description:'Award Winning.  Limited prints available',
+    dimensions:'20" x 16"'
+},
+{
+    number: 38,
+    src: 'alicepic38-500.jpg',
+    title:'A Touch of Poetry',
+    description:'In Private Collection',
+    dimensions:'16" x 16"'
+},
+{
+    number: 39,
+    src: 'alicepic39-500.jpg',
+    title:'Lotus Pond Fantasia',
+    description:'',
+    dimensions:'35" x 29"'
+},
+{
+    number: 43,
+    src: 'alicepic43-425.jpg',
+    title:'Madonna and Child',
+    description:'Award Winning.  In Private Collection.  Limited prints available',
+    dimensions:''
+},
+{
+    number: 45,
+    src: 'alicepic45-500.jpg',
+    title:'Longing for Spring',
+    description:'Award & Commission.  Large',
+    dimensions:''
+},
+{
+    number: 48,
+    src: 'alicepic48-500.jpg',
+    title:'Enduring Companionship',
+    description:'In Private Collection',
+    dimensions:''
+},
+{
+    number: 49,
+    src: 'alicepic49-500.jpg',
+    title:'Spring Mood',
+    description:'In Private Collection.  Prints available',
+    dimensions:'20" x 16"'
+},
+{
+    number: 51,
+    src: 'alicepic51-500.jpg',
+    title:'Fragrant Space',
+    description:'Flowers and bees',
+    dimensions:'17 ¾" x 17"'
+},
+{
+    number: 54,
+    src: 'alicepic54-500.jpg',
+    title:'Vineyard Vignette',
+    description:'In Private Collection.  Limited prints available',
+    dimensions:'16" x 20"'
+},
+{
+    number: 61,
+    src: 'alicepic61-500.jpg',
+    title:'Kyoto Impression',
+    description:'Two Awards.  Egret flight in Kyoto',
+    dimensions:'43½" x 36"'
+},
+{
+    number: 67,
+    src: 'alicepic67-500.jpg',
+    title:'Friendship',
+    description:'Award winning',
+    dimensions:'20" x 24"'
+},
+{
+    number: 68,
+    src: 'alicepic68-500.jpg',
+    title:'Once Upon A Time',
+    description:'Award in National Show',
+    dimensions:'34" x 25 ¾"'
+},
+{
+    number: 72,
+    src: 'alicepic72-500.jpg',
+    title:'Admiration',
+    description:'Limited prints available',
+    dimensions:''
+},
+{
+    number: 74,
+    src: 'alicepic74-500.jpg',
+    title:'Purple Mood',
+    description:'',
+    dimensions:''
+},
+{
+    number: 76,
+    src: 'alicepic76-500.jpg',
+    title:'Bamboo in Blue',
+    description:'Water media color transfer technique',
+    dimensions:'34 ½" x 29 ½"'
+},
+{
+    number: 77,
+    src: 'alicepic77-500.jpg',
+    title:'Free Spirit',
+    description:'Private collection',
+    dimensions:'34" x 28"'
+},
+{
+    number: 78,
+    src: 'alicepic78-400.jpg',
+    title:'Surge',
+    description:'Tsunami inspired',
+    dimensions:'23" x 27½"'
+},
+{
+    number: 79,
+    src: 'alicepic79-BambooChar-400.jpg',
+    title:'Tranquility',
+    description:'Caligraphy on green bamboo leaves',
+    dimensions:'8½" x 11"'
+},
+{
+    number: 80,
+    src: 'alicepic80-500.jpg',
+    title:'Pleasure in a Pond',
+    description:'Fantail goldfish in pond',
+    dimensions:'16" x 16"'
+},
+{
+    number: 81,
+    src: 'alicepic81-500.jpg',
+    title:'Mystical Path',
+    description:'An abstract',
+    dimensions:''
+},
+{
+    number: 82,
+    src: 'alicepic82-400.jpg',
+    title:'Affair with a Blossom',
+    description:'',
+    dimensions:'20" x 24"'
+},
+{
+    number: 83,
+    src: 'alicepic83-AMoment-500.jpg',
+    title:'A Moment to Cherish',
+    description:'Red Birds on Bamboo',
+    dimensions:'16" x 16"'
+},
+{
+    number: 86,
+    src: 'alicepic86-Rainbow-400.jpg',
+    title:'Rainbow Bamboo',
+    description:'Bamboo on abstract rainbow background',
+    dimensions:'15" x 20"'
+},
+{
+    number: 88,
+    src: 'alicepic88-500.jpg',
+    title:'Romance',
+    description:'In Private Collection',
+    dimensions:'20" x 12"'
+},
+{
+    number: 89,
+    src: 'alicepic89-500.jpg',
+    title:'Fragrant Mushrooms',
+    description:'One stroke accomplishes the stem with dry-brush effect.  The second stroke in gradation of ink, expressing energy flow, depicts the cap.  A showcase of brush painting.  Also note calligraphy "fragrant mushrooms" in Chinese. ',
+    dimensions:'13" x 11"'
+},
+{
+    number: 91,
+    src: 'alicepic91-500.jpg',
+    title:'A Journey Longing For',
+    description:'Landscape exploring the use of walnut ink.  Special rice paper used.',
+    dimensions:'24" x 30"'
+},
+{
+    number: 92,
+    src: 'alicepic92-500.jpg',
+    title:'A Refreshing Sensation',
+    description:'Summer leisure: colorful water lilies dotted the willow pond. What else do you see? Note: color, value, texture, composition.',
+    dimensions:'38½" x 32"'
+},
+{
+    number: 93,
+    src: 'alicepic93-500.jpg',
+    title:'Sensibility',
+    description:'Illusion; mood.',
+    dimensions:'19½" x 19½"'
+},
+{
+    number: 94,
+    src: 'alicepic94-500.jpg',
+    title:'Curiosity',
+    description:'Directed by the brilliant blue bird the discovery of traces of ink diffusion and water marks.',
+    dimensions:'24" x 20"'
+},
+{
+    number: 95,
+    src: 'alicepic95-500.jpg',
+    title:'Emotion',
+    description:'An abstract of organic forms expressing emotion through color.  Note the grass-style Chinese calligraphy,"Emotion".  Contemporary matting and framing.',
+    dimensions:'19½" x 19½"'
+},
+{
+    number: 96,
+    src: 'alicepic96-500.jpg',
+    title:'March On',
+    description:'Display of lively cicadas marching on.  Resist and layers of color-transfer techniques used.',
+    dimensions:'14¼" x 12¼"'
+},
+{
+    number: 97,
+    src: 'alicepic97-500.jpg',
+    title:'Enlightenment',
+    description:'Chinese calligraphy, "Buddha".  Note the incredible energy flow',
+    dimensions:'15 ½" x 21"'
+},
+{
+    number: 98,
+    src: 'alicepic98-500.jpg',
+    title:'A Glimpse of Light',
+    description:'Award.  Lotus leaf, bud & wild grass on Japanese "Shikishi" board.  Edge, guilt gold.',
+    dimensions:'18 ¼" x 19 ½"'
+}
+];
+
+
+GALLERY.getPaintingWithNumber = function (number) {
+  var thisPainting;
+  GALLERY.paintings.forEach(function (painting) { if (painting.number === number) { thisPainting = painting; }});
+  return thisPainting;
+};
